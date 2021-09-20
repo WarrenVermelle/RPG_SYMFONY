285700eadb5b5cf3e81eb4d0aceb6aa1d78719de
doryan fourrichon <DoryanF>
2021-09-20 15:15:50 +0200

    Ajout de la classe abstract Humanoide

--- src/Entity/Champion.php
+++ src/Entity/Champion.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use App\Entity\Abstract\Humanoide;
 use App\Repository\ChampionRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -10,19 +11,8 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity(repositoryClass=ChampionRepository::class)
  */
-class Champion
+class Champion extends Humanoide
 {
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
-    private $id;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $name;
 
     /**
      * @ORM\Column(type="smallint")
@@ -33,41 +23,6 @@ class Champion
      * @ORM\Column(type="text")
      */
     private $img;
-
-    /**
-     * @ORM\Column(type="integer")
-     */
-    private $level;
-
-    /**
-     * @ORM\Column(type="integer")
-     */
-    private $gold;
-
-    /**
-     * @ORM\Column(type="integer")
-     */
-    private $hp;
-
-    /**
-     * @ORM\Column(type="integer")
-     */
-    private $mp;
-
-    /**
-     * @ORM\Column(type="integer")
-     */
-    private $intel;
-
-    /**
-     * @ORM\Column(type="integer")
-     */
-    private $strength;
-
-    /**
-     * @ORM\Column(type="integer")
-     */
-    private $agi;
 
     /**
      * @ORM\OneToMany(targetEntity=Inventory::class, mappedBy="champ")
@@ -92,32 +47,12 @@ class Champion
      */
     private $faction;
 
-    /**
-     * @ORM\Column(type="integer")
-     */
-    private $xp;
 
     public function __construct()
     {
         $this->inventories = new ArrayCollection();
     }
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    public function setName(string $name): self
-    {
-        $this->name = $name;
-
-        return $this;
-    }
 
     public function getGender(): ?int
     {
@@ -139,90 +74,6 @@ class Champion
     public function setImg(string $img): self
     {
         $this->img = $img;
-
-        return $this;
-    }
-
-    public function getLevel(): ?int
-    {
-        return $this->level;
-    }
-
-    public function setLevel(int $level): self
-    {
-        $this->level = $level;
-
-        return $this;
-    }
-
-    public function getGold(): ?int
-    {
-        return $this->gold;
-    }
-
-    public function setGold(int $gold): self
-    {
-        $this->gold = $gold;
-
-        return $this;
-    }
-
-    public function getHp(): ?int
-    {
-        return $this->hp;
-    }
-
-    public function setHp(int $hp): self
-    {
-        $this->hp = $hp;
-
-        return $this;
-    }
-
-    public function getMp(): ?int
-    {
-        return $this->mp;
-    }
-
-    public function setMp(int $mp): self
-    {
-        $this->mp = $mp;
-
-        return $this;
-    }
-
-    public function getIntel(): ?int
-    {
-        return $this->intel;
-    }
-
-    public function setIntel(int $intel): self
-    {
-        $this->intel = $intel;
-
-        return $this;
-    }
-
-    public function getStrength(): ?int
-    {
-        return $this->strength;
-    }
-
-    public function setStrength(int $strength): self
-    {
-        $this->strength = $strength;
-
-        return $this;
-    }
-
-    public function getAgi(): ?int
-    {
-        return $this->agi;
-    }
-
-    public function setAgi(int $agi): self
-    {
-        $this->agi = $agi;
 
         return $this;
     }
@@ -289,18 +140,6 @@ class Champion
     public function setFaction(?Faction $faction): self
     {
         $this->faction = $faction;
-
-        return $this;
-    }
-
-    public function getXp(): ?int
-    {
-        return $this->xp;
-    }
-
-    public function setXp(int $xp): self
-    {
-        $this->xp = $xp;
 
         return $this;
     }
