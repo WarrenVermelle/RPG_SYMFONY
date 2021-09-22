5e43e20729693b9140297d40e1208a2f31cc3032
kevinb-dev <kevinbazin.dev@gmail.com>
2021-09-22 11:36:00 +0200

    merge create-perso sur accueil

--- assets/app.js
+++ assets/app.js
@@ -6,11 +6,7 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-<<<<<<< HEAD
 import './styles/app.scss';
-=======
-import './styles/app.css';
->>>>>>> 5efdae47203c4804838effe4a658397de9a1c817
 
 // start the Stimulus application
 import './bootstrap';
