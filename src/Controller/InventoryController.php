f961b5c74861226d2f1107308080276dab997170
WarrenVermelle <80685095+WarrenVermelle@users.noreply.github.com>
2021-09-23 14:33:30 +0200

    update inventory

--- src/Controller/InventoryController.php
+++ src/Controller/InventoryController.php
@@ -71,47 +71,48 @@ class InventoryController extends AbstractController
             // équipe l'élément cliqué
             $clickedInventoryLine->setEquiped(true);
 
-            // ajoute les caract de l'élément cliqué ou remonte pv/pm
+            // si l'élément cliqué est une potion
             if($clickedInventoryLine->getItem()->getType()->getType() === 'potion')
             {
+                // si le champion a perdu des pv ou des pm
                 if($champion->getHp() < $champion->getMaxHp() || $champion->getMp() < $champion->getMaxMp())
                 {
-                    if($clickedInventoryLine->getItem()->getHp() > $champion->getHp()){
-                        $champion->setHp($champion->getMaxHp());
-                        return $this->redirectToRoute('show_inventory');
-                    }
-                    else if($clickedInventoryLine->getItem()->getMp() > $champion->getMp())
+                    // si la potion de vie donne plus que la vie manquante
+                    if($clickedInventoryLine->getItem()->getHp() > ($champion->getMaxHp() - $champion->getHp()))
                     {
-                        $champion->setMp($champion->getMaxMp());
-                        return $this->redirectToRoute('show_inventory');
+                        // remettre au max
+                        $champion->setHp($champion->getMaxHp());
                     }
+                    // sinon si la potion de mana donne plus que la mana manquante
+                    else if($clickedInventoryLine->getItem()->getMp() > ($champion->getMaxMp() - $champion->getMp()))
+                    {
+                        // remettre au max
+                        $champion->setMp($champion->getMaxMp());
+                    }
+                    // sinon ajouter la valeur de la potion
                     else
                     {
                         $champion->setHp($champion->getHp() + $clickedInventoryLine->getItem()->getHp());
                         $champion->setMp($champion->getMp() + $clickedInventoryLine->getItem()->getMp());
-        
-                        // supprime la ligne inventaire
-                        $manager = $this->getDoctrine()->getManager();
-                        $champion->removeInventory($clickedInventoryLine,$manager);
-                        $manager->persist($champion);
-                        $manager->flush();
                     }
+                    // supprime la ligne de l'inventaire
+                    $manager = $this->getDoctrine()->getManager();
+                    $champion->removeInventory($clickedInventoryLine,$manager);
+                    $manager->persist($champion);
+                    $manager->flush();
                 }
+                // sinon retour à l'inventaire sans utiliser de potion
                 return $this->redirectToRoute('show_inventory');
             }
+            // sinon on ajoute les caract de l'objet aux stats max
             else
             {
                 $champion->setMaxHp($champion->getMaxHp() + $clickedInventoryLine->getItem()->getHp());
                 $champion->setMaxMp($champion->getMaxMp() + $clickedInventoryLine->getItem()->getMp());
+                $champion->setIntel($champion->getIntel() + $clickedInventoryLine->getItem()->getIntel());
+                $champion->setStrength($champion->getStrength() + $clickedInventoryLine->getItem()->getStrength());
+                $champion->setAgi($champion->getAgi() + $clickedInventoryLine->getItem()->getAgi());
             }
-
-            $champion->setIntel($champion->getIntel() + $clickedInventoryLine->getItem()->getIntel());
-            $champion->setStrength($champion->getStrength() + $clickedInventoryLine->getItem()->getStrength());
-            $champion->setAgi($champion->getAgi() + $clickedInventoryLine->getItem()->getAgi());
-
-            $manager = $this->getDoctrine()->getManager();
-            $manager->persist($clickedInventoryLine);
-            $manager->flush();
         }
         // si l'élément cliqué est équipé
         else
@@ -125,12 +126,11 @@ class InventoryController extends AbstractController
             $champion->setIntel($champion->getIntel() - $clickedInventoryLine->getItem()->getIntel());
             $champion->setStrength($champion->getStrength() - $clickedInventoryLine->getItem()->getStrength());
             $champion->setAgi($champion->getAgi() - $clickedInventoryLine->getItem()->getAgi());
-
-            $manager = $this->getDoctrine()->getManager();
-            $manager->persist($clickedInventoryLine);
-            $manager->persist($champion);
-            $manager->flush();
         }
+        $manager = $this->getDoctrine()->getManager();
+        $manager->persist($clickedInventoryLine);
+        $manager->persist($champion);
+        $manager->flush();
         return $this->redirectToRoute('show_inventory');
     }
 }
