469a0166f2346c3044ef7fc2b01b07d4266b147e
antho <antdebelin@gmail.com>
2021-09-23 12:20:14 +0200

    ajout style et maj des routes

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/game/voyage')]
 class MapController extends AbstractController
 {
     #[Route('/map', name: 'map')]
