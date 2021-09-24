b3a9b844bb8a780675d8cbf6ca340b84ae358c7a
kevinb-dev <kevinbazin.dev@gmail.com>
2021-09-24 16:23:11 +0200

    page_admin listuser delete_user

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/admin')]
+class AdminController extends AbstractController
+{
+    #[Route('/', name: 'admin')]
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+    #[Route('/listusers', name: 'admin_userslist')]
+    public function listUsers(UserRepository $userRepository): Response
+    {
+        return $this->render('admin/listusers.html.twig', [
+            "users" => $userRepository->findAll()
+        ]);
+    }
+
+    #[Route('/{id}', name: 'user_delete', methods: ['POST'])]
+    public function delete(Request $request, User $user): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($user);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('account_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
