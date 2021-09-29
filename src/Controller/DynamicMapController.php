85acdd94c1af9144f86e74c28f3f37e71dfef3a5
WarrenVermelle <80685095+WarrenVermelle@users.noreply.github.com>
2021-09-29 15:34:47 +0200

    create new map controller

---
+++ src/Controller/DynamicMapController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\MapRepository;
+use Doctrine\ORM\Mapping\Id;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/game/voyage')]
+class DynamicMapController extends AbstractController
+{
+    #[Route('/{id}', name: 'dynamic_map')]
+    public function index($id, MapRepository $mapRepo): Response
+    {
+        dd($mapRepo);
+
+        return $this->render('dynamic_map/index.html.twig', [
+            'controller_name' => 'DynamicMapController',
+        ]);
+    }
+}
