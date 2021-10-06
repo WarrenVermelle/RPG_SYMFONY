607ab947eaf1275328647b63a1cc7d8ea42e4684
WarrenVermelle <80685095+WarrenVermelle@users.noreply.github.com>
2021-10-06 12:04:22 +0200

    update endfight js

--- assets/controllers/combat_controller.js
+++ assets/controllers/combat_controller.js
@@ -27,42 +27,55 @@ export default class extends Controller
     {
         let btn = this.element.querySelector('button');
         let path = btn.getAttribute('data-attak');
-        await fetch(path).then((response)=>{
+
+        await fetch(path)
+        .then((response)=>{
             return response.text()
-        }).then((text)=>{
-            if(text.startsWith("\"\\")){
-                console.log(text)
+        })
+        .then((text)=>{
+
+            if(text.startsWith("\"\\"))
+            {
                 let redirectPath = JSON.parse(text);
                 localStorage.removeItem('cbtStart')
-                console.log(redirectPath)
-                //window.location.href = redirectPath;
-                fetch(redirectPath).then((response)=>{
+
+                fetch(redirectPath)
+                .then((response)=>{
                     return response.text()
-                }).then((text)=>{
-                    console.log(text)
+                })
+                .then((text)=>{
                     let parser = new DOMParser();
                     let doc = parser.parseFromString(text, 'text/html');
-                    this.element.replaceWith(doc.querySelector('#endCombat'))
-                })
-            }else{
+
+                    this.element.replaceWith(doc.querySelector('#endCombat'));
+                });
+            }
+            else
+            {
                 let parser = new DOMParser();
                 let doc = parser.parseFromString(text, 'text/html');
-                this.element.replaceWith(doc.querySelector('#startCombat'))
 
+                this.element.replaceWith(doc.querySelector('#startCombat'));
+
+                // récup les dégats subits et les soustraits aux pv
                 let hp_stats = document.querySelector('.hp_stats');
+                let damage = Number(document.querySelector('#degat').textContent);
+                let currentHealth = Number(document.querySelector('.hp_stats').textContent.match(/^[0-9]*/)[0]);
                 let target = document.querySelector('.currentHp');
-                hp_stats.innerHTML = target.getAttribute('data-hp') + ' / ' + target.getAttribute('data-max-hp');
+
+                currentHealth += damage;
+                hp_stats.innerHTML = currentHealth + ' / ' + target.getAttribute('data-max-hp');
+                // -----------------------------------------------
             }
         });
     }
     
    async endFight()
    {
-        //console.log('cc')
         window.location.href = '/game/voyage/4';
    }
 
-    async fuite(event )
+    async fuite(event)
     {
         
         let path = event.currentTarget.getAttribute('data-fuite');
